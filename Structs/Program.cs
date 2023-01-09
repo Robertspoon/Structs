54da22abcabffbc524e39e4c2b45bd43d4f9c9db
Robertspoon <robertthemcjug@gmail.com>
2023-01-09 15:09:25 -0400

    stuff

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -8,8 +8,58 @@ namespace Structs
 {
     internal class Program
     {
+        // structs
+
+        struct Vector2
+        {
+            public int x;
+            public int y;
+        }
+
+
+
+
+        //player 
+        static Vector2 position;
+        //....
+        //....
+        //....
+
+
+        //enemies 
+        static Vector2 enemy1Position;
+        //....
+        //....
+        //....
+
+        //enemies 
+        static Vector2[] enemyPositions = new Vector2[100];
+
+        struct Weapon
+        {
+           public int strength;
+           public int durability;
+           public int speed;
+        }
+
+        // weapons 
+        static Weapon sword;
+        static Weapon knife;
+        static Weapon Deatomizer;
+
+        
+
         static void Main(string[] args)
         {
+            sword.strength = 20;
+            sword.durability = 75;
+
+            Deatomizer.strength = 100;
+            Deatomizer.durability = 10;
+
+            knife.strength = 5;
+            knife.durability = 50;
+
             Console.WriteLine("Structs");
 
             Console.ReadKey(true);
